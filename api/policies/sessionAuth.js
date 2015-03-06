921d2d7b261719e80d6ff396cf55ac0251728628
Aaron Larner <anlarner@gmail.com>
2015-03-05 19:37:32 -0600

    Update sessionAuth policy to work with passport

--- api/policies/sessionAuth.js
+++ api/policies/sessionAuth.js
@@ -11,11 +11,17 @@ module.exports = function(req, res, next) {
 
   // User is allowed, proceed to the next policy, 
   // or if this is the last policy, the controller
-  if (req.session.authenticated) {
+  if (req.session.passport && req.session.passport.user) {
     return next();
   }
 
   // User is not allowed
   // (default res.forbidden() behavior can be overridden in `config/403.js`)
-  return res.forbidden('You are not permitted to perform this action.');
+  if(req.wantsJSON) {
+  	res.status(403);
+    res.jsonx({success: false, errors: ['Forbidden']});
+  }
+  else {
+  	return res.forbidden('You are not permitted to perform this action.');
+  }
 };
